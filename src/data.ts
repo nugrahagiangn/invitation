d01702188d1c42eed037d23be779c1ef53613f83
nugrahagiangn <giannugraha76@gmail.com>
2026-06-05 17:41:10 +0700

    test

--- src/data.ts
+++ src/data.ts
@@ -110,10 +110,10 @@ export const loveStories: LoveStory[] = [
   },
   {
     id: "story-3",
-    title: "Momen Lamaran (Proposal)",
+    title: "Momen Lamaran (Proposalls)",
     dateStr: "Desember 2025",
     description: "Di hadapan kedua keluarga besar, kami mengikrarkan janji suci pertunangan. Langkah penuh keberanian untuk menyatukan visi, restu tulus orang tua menuntun kami menuju pintu gerbang yang mulia ini.",
-    imageUrl: "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?auto=format&fit=crop&q=80&w=800"
+    imageUrl: "url('/wedding/images/lm.jpeg')?auto=format&fit=crop&q=80&w=800"
   },
   {
     id: "story-4",
